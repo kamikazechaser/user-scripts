0a1f0f730d3a087d84e1d533a55d64044d7a95ee
Mohammed Sohail <sohailsameja@gmail.com>
2020-12-16 15:52:40 +0300

    Create shodan-ip-dump.js

---
+++ shodan-ip-dump.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Shodan IP Dump
+// @namespace    kamikazechaser/shodan-ip-dump
+// @version      0.1
+// @description  Dump all IPs on a Shodan serach page for easier selecting
+// @author       kamikazechaser
+// @match        https://www.shodan.io/search?query=*
+// @grant        none
+// ==/UserScript==
+
+const ipLinks = [];
+
+$(document).ready(function() {
+    const $textArea = $("<br/><textarea id=ipList></textarea>").appendTo(".span3");
+    $(".ip").each(function(i, e) {
+        ipLinks.push($(this).find("a").text());
+    });
+  
+    $("#ipList").val(ipLinks.join(" "));
+});
